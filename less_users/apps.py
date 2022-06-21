b80e47b322e36a116e48e9a2568a27a4bf0e0494
Justyna Łuczak <justyna.macniak@gmail.com>
2022-06-21 13:40:18 +0200

    signals for create and update user profile

--- less_users/apps.py
+++ less_users/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class LessUsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'less_users'
+
+    def ready(self):
+        import less_users.signals
