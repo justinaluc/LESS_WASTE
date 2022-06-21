f44c844373787c3a9e8cd54c25e701ef6cd29837
Justyna Łuczak <justyna.macniak@gmail.com>
2022-06-21 15:11:12 +0200

    create LOGIN-LOGOUT url and template.html with navbar links

--- less_users/urls.py
+++ less_users/urls.py
@@ -1,8 +1,12 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
+
 from less_users.views import HomeView, RegisterView, ProfileView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='less_users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='less_users/logout.html'), name='logout'),
 ]
