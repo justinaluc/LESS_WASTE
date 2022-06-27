0db00eaccb366ba5fe03d4e22477c2ea84a4abd1
Justyna Łuczak <justyna.macniak@gmail.com>
2022-06-28 00:01:01 +0200

    admin site with challenges and their categories with possibility of management by admin panel by filtering, ordering, displaying only necessary model elements

--- challenges/admin.py
+++ challenges/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Challenge, Category, CategoryChallenge
+
+
+class CategoryChallengeInline(admin.TabularInline):
+    model = CategoryChallenge
+    extra = 1
+
+
+class ChallengeAdmin(admin.ModelAdmin):
+    # add many2many relation field into list
+    inlines = (CategoryChallengeInline,)
+
+    # show below data in challenge list
+    list_display = ('name', 'duration', 'frequency', 'points')
+
+    # add filters for frequency and points
+    list_filter = ('frequency', 'points')
+
+    # sort list alphabetically by 'name'
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    # add many2many relation field into list
+    inlines = (CategoryChallengeInline,)
+
+
+class CategoryChallengeAdmin(admin.ModelAdmin):
+    # show below data in category challenge list
+    list_display = ('category', 'challenge')
+
+    # add filter for category
+    list_filter = ('category',)
+
+
+admin.site.register(Challenge, ChallengeAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(CategoryChallenge, CategoryChallengeAdmin)
+
