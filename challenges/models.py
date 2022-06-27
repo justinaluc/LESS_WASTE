ac45c7bcd5551fce03c30eeae8089923ca15bffb
Justyna Łuczak <justyna.macniak@gmail.com>
2022-06-27 14:03:01 +0200

    create CHALLENGES app with models, migrations, register in settings.py installed_apps

---
+++ challenges/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Challenge(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.IntegerField(help_text='how long it takes to complete challenge')
+    frequency = models.IntegerField(help_text='how often challenge is activated: 1/day, 1/week, 1/month, 1/year')
+    points = models.IntegerField(help_text='how many points are gained each time challenge is completed')
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    challenges = models.ManyToManyField(Challenge, through="CategoryChallenge")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class CategoryChallenge(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    challenge = models.ForeignKey(Challenge, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.category.name + ': ' + self.challenge.name
+
+    class Meta:
+        verbose_name_plural = "Challenges categories"
