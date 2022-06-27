0504f1598761cf67a4099bec5c409429515833a2
Justyna Łuczak <justyna.macniak@gmail.com>
2022-06-27 23:13:39 +0200

    generic views of listed and detailed categories and listed and detailed challenges, with urls and navbar links; still content in db needed

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
 
+from challenges.views import CategoryListView, ChallengeListView, CategoryDetailView, ChallengeDetailView
+
 urlpatterns = [
+    path('category/', CategoryListView.as_view(), name='category_list'),
+    path('category/<int:pk>', CategoryDetailView.as_view(), name='category_detail'),
+    path('challenge/', ChallengeListView.as_view(), name='challenge_list'),
+    path('challenge/<int:pk>', ChallengeDetailView.as_view(), name='challenge_detail'),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Challenge, Category
+# from less_users.models import UserChallenge, Profile, Log
+
+# views for CATEGORY functionalities:
+
+
+class CategoryListView(ListView):
+    """generic ListView to list all categories of challenges"""
+    model = Category
+    ordering = ['name']
+
+
+class CategoryDetailView(DetailView):
+    """generic DetailView to show challenges in particular category"""
+    model = Category
+
+# views for CHALLENGE functionalities:
+
+
+class ChallengeListView(ListView):
+    """generic ListView to list all categories of challenges"""
+    model = Challenge
+    ordering = ['name']
+    paginate_by = 12
+
+
+class ChallengeDetailView(DetailView):
+    """generic DetailView to show category with its details"""
+    model = Challenge
+
