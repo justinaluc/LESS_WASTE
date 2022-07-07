acaf144df1984b8386f9b0d1213585db5ae012da
Justyna Łuczak <justyna.macniak@gmail.com>
2022-07-07 10:49:41 +0200

    Revert "functionality of activating user_challenge for logged in user and particular challenge both from ChallengeDetailView and ChallengeListView by HttpResponseRedirect reverse"
    This reverts commit fa7c195f388bc0ee8d82cda4d3337d2ed086d948.

--- challenges/views.py
+++ challenges/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 
-from less_users.models import UserChallenge
 from .models import Challenge, Category
 # from less_users.models import UserChallenge, Profile, Log
 
@@ -33,12 +32,4 @@ class ChallengeListView(ListView):
 class ChallengeDetailView(DetailView):
     """generic DetailView to show category with its details"""
     model = Challenge
-
-    # def get_context_data(self, *args, **kwargs):
-    # """add to context information about active challenge for logged in user"""
-    #     active = UserChallenge.objects.filter(user=self.request.user.id)
-    #     context = super(ChallengeDetailView, self).get_context_data()
-    #     context['active'] = active
-    #     return context
-    #
 
