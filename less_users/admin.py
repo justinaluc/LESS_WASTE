085286cd95c3fc127b7bc3fa6164cce37d19cf6e
Justyna Łuczak <justyna.macniak@gmail.com>
2022-07-07 10:47:10 +0200

    fast changes with buttons noticed

--- less_users/admin.py
+++ less_users/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Profile
+
+from .models import Profile, UserChallenge
 
 admin.site.register(Profile)
+
+
+class UserChallengeAdmin(admin.ModelAdmin):
+    # show below data in user challenge list
+    list_display = ('user', 'challenge', 'is_active', 'start_date')
+
+    # add filter for user
+    list_filter = ('user',)
+
+
+admin.site.register(UserChallenge, UserChallengeAdmin)
