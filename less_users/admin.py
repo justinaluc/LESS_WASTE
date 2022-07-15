4a4baba9ee6a487fe905ebd4a417036a4e85b5b3
Justyna Łuczak <justyna.macniak@gmail.com>
2022-07-15 13:28:14 +0200

    improving admin view of UserChallenges by Logs and Profile by not editable fields (readonly)

--- less_users/admin.py
+++ less_users/admin.py
@@ -5,18 +5,8 @@ from .models import Profile, UserChallenge, Log
 
 class LogInline(admin.TabularInline):
     model = Log
-    extra = 2
-
-
-class UserChallengeAdmin(admin.ModelAdmin):
-    # add many2many relation field into list
-    inlines = (LogInline,)
-
-    # show below data in user challenge list
-    list_display = ('user', 'challenge', 'is_active', 'start_date')
-
-    # add filter for user
-    list_filter = ('user',)
+    extra = 1
+    readonly_fields = ['date', 'points']
 
 
 class LogAdmin(admin.ModelAdmin):
@@ -25,6 +15,27 @@ class LogAdmin(admin.ModelAdmin):
     list_display = ('user_challenge', 'date', 'points')
 
 
-admin.site.register(Profile)
+class UserChallengeAdmin(admin.ModelAdmin):
+    # add many2many relation field into list
+    inlines = (LogInline,)
+
+    # show below data in user challenge list
+    list_display = ('user', 'challenge', 'is_active', 'start_date')
+    readonly_fields = ['user', 'challenge', 'start_date']
+    # add filter for user
+    list_filter = ('user',)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    # add django-nested-admin into profile view (user/ user_challenge/ logs)
+
+    # show below data in profile list
+    list_display = ('user', 'points')
+
+    readonly_fields = ['user', 'points']
+
+
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(UserChallenge, UserChallengeAdmin)
 admin.site.register(Log, LogAdmin)
+
