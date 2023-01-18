fa0f18d750f493eabd34528e5e677b25088bf72e
Justyna Łuczak <justyna.macniak@gmail.com>
2023-01-18 13:26:41 +0100

    test for user profile

--- less_users/tests/test_user_profile.py
+++ less_users/tests/test_user_profile.py
@@ -1,8 +1,4 @@
-from less_users.models import Profile
-from less_users.forms import (
-    UserRegisterForm,
-    UserUpdateForm,
-)  # do przyszłego wykorzystania
+from less_users.models import Profile, UserChallenge, Log
 
 
 def test_create_user_profile(user):
@@ -21,3 +17,17 @@ def test_change_user_profile(user):
 
 def test_change_user_profile_by_user_update_form(user):
     pass
+
+
+def test_change_user_profile_points_by_clicking_done(user, challenge_2_week):
+    my_profile = Profile.objects.get(user_id=user.id)
+    user_challenge = UserChallenge.objects.create(user=user, challenge=challenge_2_week)
+    user_challenge.check_if_active()
+    points = challenge_2_week.points
+    if user_challenge.get_points() == points and user_challenge.is_active:
+        new_log = Log.objects.create(user_challenge_id=user_challenge.id, points=points)
+        my_profile.points += points
+        my_profile.save()
+
+    assert my_profile.points == points
+
